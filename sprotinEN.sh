2a08f4f118bece67e59821f45a63306b37c21994
flakvard <marni@falkvard.com>
2023-07-20 17:55:46 +0200

    update

--- sprotinEN.sh
+++ sprotinEN.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
+# Sleep for a short period to allow time to switch to the desired window or field
+sleep 0.5
 
-# Copy selected text to clipboard using xclip
+# Copy the selected text to the clipboard using xclip
 selected_text=$(xclip -o -selection primary)
 
-# Run the Python script with the selected text as an argument
-python3 insert_text.py "$selected_text"
+# Open Chrome with the specified URL
+google-chrome "https://sprotin.fo/dictionaries?_SearchInflections=0&_SearchDescriptions=0&_DictionaryId=3&_l=fo&_DictionaryPage=1&_SearchFor=${selected_text}"
+
+# Wait for a short period to let the page load before pressing Enter
+sleep 2
+
+# Simulate pressing Enter using xdotool
+xdotool key --clearmodifiers "Return"
+
